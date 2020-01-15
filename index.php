56cf9467367abebbbdf362e5ce0b6d2fc83fc930
Backend Developer <kivanc.inci83@gmail.com>
2020-01-15 15:04:28 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,7 @@
 <?php 
-phpinfo();
+date_default_timezone_set('Europe/Istanbul');
+require_once("autoloader.php");
+$route= new Route();
+$route->GET("/", function () {
+      echo "<h4>Merhaba</h4>";
+});
